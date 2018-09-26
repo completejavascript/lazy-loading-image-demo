dae67c1564c9aa947186c29f11db0719667a3cd8
completejavascript <completejavascript.super@gmail.com>
2018-09-26 20:26:36 +0700

    add folkme

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LazyImage from './lazy-image';
+import FolkMe from './Folkme';
 import './App.css';
 import placeHolder from '../images/place-holder.png';
 import img1 from '../images/01.jpg';
@@ -42,25 +43,34 @@ class App extends Component {
         <h2 className="title">Scroll down to check out these beautiful girls...</h2>
 
         <div className="grid">
-          <LazyImageWrapper src={img1} alt={`img 1`}/>
-          <LazyImageWrapper src={img2} alt={`img 2`}/>
-          <LazyImageWrapper src={img3} alt={`img 3`}/>
-          <LazyImageWrapper src={img4} alt={`img 4`}/>
-          <LazyImageWrapper src={img5} alt={`img 5`}/>
-          <LazyImageWrapper src={img6} alt={`img 6`}/>
-          <LazyImageWrapper src={img7} alt={`img 7`}/>
-          <LazyImageWrapper src={img8} alt={`img 8`}/>
-          <LazyImageWrapper src={img9} alt={`img 9`}/>
-          <LazyImageWrapper src={img10} alt={`img 10`}/>
-          <LazyImageWrapper src={img11} alt={`img 11`}/>
-          <LazyImageWrapper src={img12} alt={`img 12`}/>
-          <LazyImageWrapper src={img13} alt={`img 13`}/>
-          <LazyImageWrapper src={img14} alt={`img 14`}/>
-          <LazyImageWrapper src={img15} alt={`img 15`}/>
-          <LazyImageWrapper src={img16} alt={`img 16`}/>
-          <LazyImageWrapper src={img17} alt={`img 17`}/>
-          <LazyImageWrapper src={img18} alt={`img 18`}/>
+          <LazyImageWrapper src={img1} alt={`img 1`} />
+          <LazyImageWrapper src={img2} alt={`img 2`} />
+          <LazyImageWrapper src={img3} alt={`img 3`} />
+          <LazyImageWrapper src={img4} alt={`img 4`} />
+          <LazyImageWrapper src={img5} alt={`img 5`} />
+          <LazyImageWrapper src={img6} alt={`img 6`} />
+          <LazyImageWrapper src={img7} alt={`img 7`} />
+          <LazyImageWrapper src={img8} alt={`img 8`} />
+          <LazyImageWrapper src={img9} alt={`img 9`} />
+          <LazyImageWrapper src={img10} alt={`img 10`} />
+          <LazyImageWrapper src={img11} alt={`img 11`} />
+          <LazyImageWrapper src={img12} alt={`img 12`} />
+          <LazyImageWrapper src={img13} alt={`img 13`} />
+          <LazyImageWrapper src={img14} alt={`img 14`} />
+          <LazyImageWrapper src={img15} alt={`img 15`} />
+          <LazyImageWrapper src={img16} alt={`img 16`} />
+          <LazyImageWrapper src={img17} alt={`img 17`} />
+          <LazyImageWrapper src={img18} alt={`img 18`} />
         </div>
+
+        <FolkMe
+          targetURL={this.props.url}
+          color="#fff"
+          backgroundColor="#000"
+          position="right"
+          size="100px"
+          ariaLabel="View source on Github"
+        />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App url="https://github.com/completejavascript/lazy-loading-image-demo" />, 
+  document.getElementById('root')
+);
